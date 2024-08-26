90ea76866ebdfbc5916f84a2d4b34ebd73fa3483
James Christou <james.christou@gmail.com>
2024-08-26 22:05:08 +1000

    Adding tests with inline data to see what happens

--- SampleTests/Tests.cs
+++ SampleTests/Tests.cs
@@ -2,6 +2,7 @@ namespace SampleTests
 {
     public class Tests
     {
+        [Trait("Category", "Group1")]
         [Fact]
         public void TestThatRandomlyFails()
         {
@@ -10,6 +11,7 @@ namespace SampleTests
             Assert.Equal(1, rnd.Next(2));
         }
 
+        [Trait("Category", "Group1")]
         [Fact]
         public void TestThatRandomlyFails2()
         {
@@ -18,6 +20,7 @@ namespace SampleTests
             Assert.Equal(1, rnd.Next(2));
         }
 
+        [Trait("Category", "Group1")]
         [Fact]
         public void TestThatRandomlyFails3()
         {
@@ -26,12 +29,35 @@ namespace SampleTests
             Assert.Equal(1, rnd.Next(2));
         }
 
+        [Trait("Category", "Group1")]
         [Fact]
         public void TestThatRandomlyFails4()
         {
             Random rnd = new Random();
 
             Assert.Equal(1, rnd.Next(2));
+        }
+
+        [Trait("Category", "Group1")]
+        [Theory]
+        [InlineData(1, 2)]
+        [InlineData(2, 3)]
+        public void TestThatRandomlyFails5(int start, int testNum)
+        {
+            Random rnd = new Random();
+
+            Assert.Equal(start, rnd.Next(testNum));
+        }
+
+        [Trait("Category", "Group1")]
+        [Theory]
+        [InlineData(1, 2)]
+        [InlineData(2, 3)]
+        public void TestThatRandomlyFails6(int start, int testNum)
+        {
+            Random rnd = new Random();
+
+            Assert.Equal(start, rnd.Next(testNum));
         }
     }
 }

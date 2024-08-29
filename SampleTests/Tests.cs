d6530d206ace15f5645e94cd7c6e2edb0d65aed5
James Christou <james.christou@gmail.com>
2024-08-29 18:45:59 +1000

    Adding a new test that uses MemberData and has a ridiculously long name whose displayname gets truncated in test results output file

--- SampleTests/Tests.cs
+++ SampleTests/Tests.cs
@@ -59,5 +59,50 @@ namespace SampleTests
 
             Assert.Equal(start, rnd.Next(testNum));
         }
+
+        [Trait("Category", "Group1")]
+        [Theory]
+        [MemberData(nameof(InvalidInputReturnsErrorMessage))]
+        public void TestThatRandomlyFails7_WithARidiculouslyLongTestingNameThatWillExceedTheCharacterLimitAtWhichXUnitTruncatesTestDisplayName(InputModel inputModel, string message)
+        {
+            Random rnd = new Random();
+
+            var errorMessages = new List<string>()
+            {
+                "This is an error that you would expect",
+                "This is a different error that you might expect"
+            };
+
+            Assert.Equal(message, errorMessages[rnd.Next(inputModel.InputA)]);
+        }
+
+        
+        public static IEnumerable<object[]> InvalidInputReturnsErrorMessage()
+        {
+            yield return new object[]
+            {
+                new InputModel
+                {
+                    InputA = 1, InputB = false, InputC = true
+                },
+                "This is an error that you would expect"
+            };
+
+            yield return new object[]
+            {
+                new InputModel
+                {
+                    InputA = 2, InputB = false, InputC = true
+                },
+                "This is a different error that you might expect"
+            };
+        }
+
+        public class InputModel
+        {
+            public int InputA { get; set; }
+            public bool InputB { get; set; }
+            public bool InputC { get; set; }
+        }
     }
 }
